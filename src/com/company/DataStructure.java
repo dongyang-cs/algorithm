bb0a6711e40e36f432b90290ce3fa87079863011
arvin <meyeal@163.com>
2017-03-26 17:12:21 +0800

    update shell

--- src/com/company/DataStructure.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-/**
- * Created by Administrator on 2017/2/19.
- */
-public interface DataStructure<K> {
-    void add(K k);
-
-}
--- src/me/alfod/datastructure/tree/Tree/RedBlackTree.java
+++
@@ -1,8 +0,0 @@
-package me.alfod.datastructure.tree.Tree;
-
-/**
- * Created by Administrator on 2017/2/9.
- */
-public class RedBlackTree {
-
-}
