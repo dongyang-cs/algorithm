18f7d1b6fa4571364e6da02e2b01024e58216107
arvin <meyeal@163.com>
2017-04-04 10:01:38 +0800

    update shell

--- src/main/java/me/alfod/algorithm/sort/Shell.java
+++ src/main/java/me/alfod/algorithm/sort/Shell.java
@@ -8,39 +8,55 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Shell {
-    private static int[] getShellIncrementArrayByName(String incrementName, int arrayLength) {
+    /**
+     * @param incrementName increment sequence name
+     * @param arrayLength   sorted array`s length
+     * @return an increment sequence which length is half of the array`length
+     */
+    public static Integer[] getIncrementSequenceByName(String incrementName, int arrayLength) {
+        final String defaultSequenceName = "sedgewick";
         switch (incrementName) {
-            case "Sedgewick": {
-                int i = 1, j = 1;
-                List<Integer> linkedList = new LinkedList<>();
-                while (i < arrayLength / 2) {
-                    linkedList.add(i);
-                    j++;
-                    i = (int) (Math.pow(4, j) - 3 * Math.pow(2, j) + 1);
-                }
-                Integer[] a = linkedList.toArray(new Integer[linkedList.size()]);
-                int[] b = new int[linkedList.size()];
-                for (i = 0; i < b.length; i++) {
-                    b[i] = a[i];
-                }
-                return b;
+            case "sedgewick": {
+                int sequenceElement = 0, index = 1;
+                final int quarterLength = arrayLength / 4;
 
+                List<Integer> linkedList = new LinkedList<>();
+                linkedList.add(1);
+                while (sequenceElement < quarterLength) {
+
+                    sequenceElement = (int) (Math.pow(4, index + 1) - 3 * Math.pow(2, index + 1) + 1);
+                    linkedList.add(sequenceElement);
+                    System.out.println(sequenceElement);
+
+                    sequenceElement = (int) (9 * Math.pow(4, index) - 9 * Math.pow(2, index) + 1);
+                    linkedList.add(sequenceElement);
+                    System.out.println(sequenceElement);
+
+                    index++;
+                }
+                Integer[] sequence = linkedList.toArray(new Integer[linkedList.size()]);
+                return sequence;
+
+            }
+            default: {
+                return getIncrementSequenceByName(defaultSequenceName, arrayLength);
             }
         }
 
-        return null;
     }
 
 
     public static <T extends Comparable<? super T>> T[] shell(T[] inputArray) {
-        return shell(inputArray, true);
+        return shell(inputArray, true, "sedgewick");
     }
 
-    public static <T extends Comparable<? super T>> T[] shell(T[] inputArray, boolean isAsc) {
-        int sequence[] = getShellIncrementArrayByName("Sedgewick", inputArray.length);
+    private static <T extends Comparable<? super T>> T[] shell(T[] inputArray, final boolean isAsc, final String sequenceName) {
+
+        Integer[] sequence = getIncrementSequenceByName(sequenceName, inputArray.length);
+
         int tmpSecondForIndex, currentSequenceElement;
         T currentValue;
-        boolean order = true;
+        boolean order;
         int firstForIndex, secondForIndex;
         for (firstForIndex = sequence.length - 1; firstForIndex >= 0; firstForIndex--) {
             currentSequenceElement = sequence[firstForIndex];
@@ -48,23 +64,35 @@ public class Shell {
                 tmpSecondForIndex = secondForIndex;
                 currentValue = inputArray[secondForIndex * currentSequenceElement];
 
-                while (tmpSecondForIndex > 0 && inputArray[(tmpSecondForIndex - 1) * currentSequenceElement].compareTo(currentValue) > 0) {
+                while (tmpSecondForIndex > 0) {
+
+                    if (isAsc) {
+                        order = inputArray[(tmpSecondForIndex - 1) * currentSequenceElement].compareTo(currentValue) > 0;
+                    } else {
+                        order = inputArray[(tmpSecondForIndex - 1) * currentSequenceElement].compareTo(currentValue) < 0;
+                    }
+                    if (!order) {
+                        break;
+                    }
                     inputArray[tmpSecondForIndex * currentSequenceElement] = inputArray[(tmpSecondForIndex - 1) * currentSequenceElement];
                     --tmpSecondForIndex;
+
                 }
                 inputArray[tmpSecondForIndex * currentSequenceElement] = currentValue;
+
             }
         }
-        
+
         return inputArray;
 
     }
 
     public static void main(String[] args) {
-        Integer[] tmps = {1, 3, 2, 6, 3, 5, 0, 9, 4, 8};
-        tmps = Shell.shell(tmps);
-        for (int i = 0; i < tmps.length; i++) {
-            System.out.println(tmps[i]);
-        }
+        getIncrementSequenceByName("sedgewick", 10000);
+//        Integer[] tmps = {1, 3, 5, 7, 9, 0, 2, 4, 6, 8};
+//        tmps = Shell.shell(tmps);
+//        for (int i = 0; i < tmps.length; i++) {
+//            System.out.println(tmps[i]);
+//        }
     }
 }
